33f418764cd0e4dc7aafcf94f9aeb4d537ba300f
Im-Alam <imranalam04418@gmail.com>
2024-12-02 11:02:42 +0530

    all method allowed

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ app = FastAPI(version="1.0.0", title="Backend intern hiring task")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ['*'],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
