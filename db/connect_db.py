c014f62ddd112a119a2e1371444d1dd22d3d2356
Im-Alam <imranalam04418@gmail.com>
2024-12-03 15:04:31 +0530

    connect to db using early method

--- db/connect_db.py
+++ db/connect_db.py
@@ -4,30 +4,21 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv()
 
-class MongoDB:
-    client: AsyncIOMotorClient = None
 
-    @classmethod
-    def connectDB(cls):
-        if not cls.client:
-            uri = os.getenv('db_connection_str')
-            cls.client = AsyncIOMotorClient(uri)
-        return cls.client
+def connectDB():
+    uri = os.getenv('db_connection_str')
 
-    @classmethod
-    async def ping(cls):
-        try:
-            await cls.client.admin.command('ping')
-            print("Successfully connected to MongoDB!")
-        except Exception as e:
-            print(f"Error connecting to MongoDB: {e}")
+    client = AsyncIOMotorClient(uri)
 
+    try:
+        client.admin.command('ping')
+        print("Successfully connected to MongoDB!")
+        return client
+    except Exception as e:
+        print(e)
 
-# Initialize the database connection
-db_client = MongoDB.connectDB()
-students_collection = db_client['students_db']['students']
+client = connectDB()
+students_collection = client['students_db']['students']
 
-# Test the connection
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(MongoDB.ping())
+    connectDB()
